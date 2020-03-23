aaa71422d51c2855d20c13dd3d0627a0f150cfa1
oopsy <whoops.yao@gmail.com>
2020-03-23 23:08:13 +0800

    🚧 SDE H3 code from wikipedia

--- SDE/hw3/simulator.py
+++ SDE/hw3/simulator.py
@@ -2,23 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-T = 100
-steps = 1000
+T = 50
+steps = 500
+dt = T / steps
+np.random.seed(1)
 # leaps = (T / steps) ** 0.5 * np.random.randn(steps)
 
 def sample_path_generator(mu, sigma):
-    leaps = (T / steps) ** 0.5 * np.random.randn(steps)
+    leaps = dt ** 0.5 * np.random.randn(steps)
     X = [1]
     last = X[0]
-    t = 0
     for mov in leaps:
-        t += T / steps
-        last *= math.exp(mu * t + sigma * mov)
+        last *= math.exp(mu * dt + sigma * mov)
         X.append(last)
     return X
 
-for mu, sigma in [(0.00001, 0.1), (-0.00001, 0.1), (-0.00001, 0.2)]:
+for mu, sigma in [(0.00001, 1), (-0.00001, 1), (-0.00001, 1.5)]:
     plt.plot(np.linspace(0, T, 1 + steps), sample_path_generator(mu,sigma), label=f'$\mu={mu},\sigma={sigma}$')
+
 plt.legend()
 plt.xlabel('t')
 plt.ylabel('X')
