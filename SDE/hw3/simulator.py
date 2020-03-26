a44228933f3e83bf119c19d5097e1c30a6323e8a
oopsy <whoops.yao@gmail.com>
2020-03-26 16:25:46 +0800

    🚧 SDE H3P5

--- SDE/hw3/simulator.py
+++ SDE/hw3/simulator.py
@@ -2,25 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-T = 50
-steps = 500
-dt = T / steps
+T = 10
+dt = 0.001
+steps = int(T / dt)
 np.random.seed(1)
-# leaps = (T / steps) ** 0.5 * np.random.randn(steps)
 
-def sample_path_generator(mu, sigma):
-    leaps = dt ** 0.5 * np.random.randn(steps)
-    X = [1]
-    last = X[0]
-    for mov in leaps:
-        last *= math.exp(mu * dt + sigma * mov)
-        X.append(last)
-    return X
+def sample_path_generator(mu, sigma, x0=1):
+    # Independent dx
+    dx = np.exp(
+        mu * dt
+        + sigma * np.random.normal(0, np.sqrt(dt), steps)
+    )
+    dx = np.hstack([np.ones(1), dx])
+    # Their product as accumulation
+    x = x0 * dx.cumprod(axis=0)
+    return x
 
-for mu, sigma in [(0.00001, 1), (-0.00001, 1), (-0.00001, 1.5)]:
-    plt.plot(np.linspace(0, T, 1 + steps), sample_path_generator(mu,sigma), label=f'$\mu={mu},\sigma={sigma}$')
+for mu, sigma in [(1, 0.4), (1, 0.8), (0.5, 0.4), (-0.3, 0.4)]:
+    path = sample_path_generator(mu, sigma)
+    plt.plot(np.linspace(0, T, 1 + steps), path, label=f'$\mu={mu},\sigma={sigma}$')
 
 plt.legend()
 plt.xlabel('t')
 plt.ylabel('X')
+plt.title(f'$T = {T}$')
 plt.show()
