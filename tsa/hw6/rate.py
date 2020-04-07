fc96d6e44d6f4b36235660ecff4a63a78ebdc798
oopsy <whoops.yao@gmail.com>
2020-04-07 17:36:57 +0800

    🚧 TSA H6 AR

--- tsa/hw6/rate.py
+++ tsa/hw6/rate.py
@@ -1,12 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import statsmodels.api as sm
+import statsmodels.tsa.stattools as stattools
+from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from arch.univariate import ARX, GARCH
+from arch import arch_model
 
 data = pd.read_excel('hw6/NYSEReturns.38135430_第三章.xlsx')
-X = data['RATE'].dropna().to_numpy()
+rates = data['RATE'].dropna().to_numpy()
 
-plt.plot(X)
-sm.graphics.tsa.plot_acf(X)
-sm.graphics.tsa.plot_pacf(X)
+# Statistic description
+plt.plot(rates)
+sm.graphics.tsa.plot_acf(rates)
 
+# ARCH effect
+ar_res = AutoReg(rates, lags=[1]).fit()
+# Test of no serial correlation and homoskedasticity
+print(ar_res.diagnostic_summary())
+
+a = ar_res.resid
+a_res = ar_select_order(a, 5).model.fit()
+print(a_res.diagnostic_summary())
+
+# Fit with GARCH(p, q)
+ar = ARX(rates, lags=[1])  # Mean model
+ar.volatility = GARCH(p=4, q=1)  # Volatility model
+
+res = ar.fit()
+res.plot()
+# plt.plot(res.resid)
+print(res.summary())
 plt.show()
