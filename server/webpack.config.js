05d7087eb7b4dbac65a2c0a1000eee9d0b732327
oopsy <whoops.yao@gmail.com>
2020-04-11 20:02:37 +0800

    ✨ Server

---
+++ server/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ]
+};
