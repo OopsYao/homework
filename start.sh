18e1f9cbef9f86e6c994821846c4c16385bff93e
oopsy <whoops.yao@gmail.com>
2020-04-30 12:59:30 +0800

    ⚗ Building Chinese doc

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
+echo 'Start building SDE'
 source build-sub.sh SDE
+
+echo 'Start building FA'
+source build-sub.sh functional-analysis
+
+echo 'Start building TSA'
+source build-sub.sh tsa
+
+echo 'Start building StCal'
+source build-sub.sh st-cal
 
 # Do the cleanning
 WORKSPACE=$PWD
