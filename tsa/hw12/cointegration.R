fa7af8dd7bb08af8ec4aa362976a08c58651aef6
oopsy <whoops.yao@gmail.com>
2020-05-13 19:38:31 +0800

    🚧 TSA H12 ADF test

--- tsa/hw12/cointegration.R
+++ tsa/hw12/cointegration.R
@@ -5,6 +5,12 @@ data <- read.xlsx("hw12/quarterly.7775706_第一章.xlsx")
 design <- na.omit(data[, c("Tbill", "r5", "r10")])
 
 t_bill <- data.matrix(design["Tbill"])
+r5 <- data.matrix(design["r5"])
+r10 <- data.matrix(design["r10"])
 r <- data.matrix(design[, c("r5", "r10")])
+
+adf.test(t_bill)
+adf.test(r5)
+adf.test(r10)
 
 coint.test(t_bill, r)
