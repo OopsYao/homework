ecf7ae212b6fef45c4d2bd4ebaeaff36cfe831f3
oopsy <whoops.yao@gmail.com>
2020-05-15 10:03:35 +0800

    🚧 TSA H12 Cointegration test

--- tsa/hw12/cointegration.R
+++ tsa/hw12/cointegration.R
@@ -1,5 +1,5 @@
-library("openxlsx")
-library("aTSA")
+library(openxlsx)
+library(ggplot2)
 
 data <- read.xlsx("hw12/quarterly.7775706_第一章.xlsx")
 design <- na.omit(data[, c("Tbill", "r5", "r10")])
@@ -9,8 +9,20 @@ r5 <- data.matrix(design["r5"])
 r10 <- data.matrix(design["r10"])
 r <- data.matrix(design[, c("r5", "r10")])
 
+index <- seq_len(nrow(design))
+ggplot(data.frame(index, design)) +
+    geom_point(aes(index, r5)) +
+    geom_point(aes(index, r10))
+
+# ADF test for the 3 sequences
 adf.test(t_bill)
 adf.test(r5)
 adf.test(r10)
 
-coint.test(t_bill, r)
+# Direct regression
+coint_lm <- lm(t_bill ~ r5 + r10)
+coint_res <- resid(coint_lm)
+
+# ADF test of residual
+adf.test(coint_res)
+# plot(coint_res, main = "Residual of Regression", pch = 19, frame = FALSE)
