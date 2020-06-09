c86628431ee1a8333e706497fc51d11d915059b1
oopsy <whoops.yao@gmail.com>
2020-06-09 12:00:09 +0800

    ⚡️ TSA W15P1 Fix test

--- tsa/wk15/var.py
+++ tsa/wk15/var.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from statsmodels.tsa.base.datetools import dates_from_str
 import numpy as np
+
+from arch.unitroot import ADF, PhillipsPerron
 from statsmodels.tsa.api import VAR
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -16,16 +19,39 @@ mdata['IndProd'] = np.log(mdata['IndProd']).diff()
 mdata['Unemp'] = mdata['Unemp'].diff()
 mdata = mdata.drop(['r10', 'Tbill'], axis=1).dropna()
 
-results = VAR(mdata).fit(ic='bic', verbose=True)
+# ADF Test
+print(ADF(mdata['r']).summary())
+print(ADF(mdata['IndProd']).summary())
+print(ADF(mdata['Unemp']).summary())
+
+# VAR fit (no constant term)
+results = VAR(mdata).fit(ic='bic', verbose=True, trend='nc')
 results.plot()
-results.plot_acorr()
 print(results.summary())
 # Selected lag order
-print(results.k_ar)
+print('Selected Order:', results.k_ar)
 
 # AIC & BIC of different lags
 for p in range(8):
-    res = VAR(mdata).fit(p)
+    res = VAR(mdata).fit(p, trend='nc')
     print(res.k_ar, '&', res.aic, '&', res.bic, '\\\\')
+
+# Stability
+print(results.is_stable(True))
+
+# Residual normality
+print(results.test_normality().summary())
+
+# Granger causality
+names = ['r', 'IndProd', 'Unemp']
+for i in range(len(names)):
+    n = names[i]
+    print('Granger for', n)
+    # Factor `n` caused by its complement
+    print(results.test_causality(n, list(set(names) - set(n))).summary())
+
+# Impulse Response Analysis
+irf = results.irf(10)
+irf.plot(orth=False)
 
 plt.show()
