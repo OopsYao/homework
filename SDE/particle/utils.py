1035b83d7efe9d7eaec3bb19b9f104ea78e68806
oopsy <whoops.yao@gmail.com>
2020-06-21 23:26:02 +0800

    ✅ SDE Add theta test

--- SDE/particle/utils.py
+++ SDE/particle/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numpy import linalg as LA
+import unittest
+import numpy.testing as npt
 
 
 def delta_matrix(x):
@@ -17,21 +19,52 @@ def norm(matrix, p=1):
 def theta_matrix(x, y):
     xy = x @ y.T
     nor = norm(x) * (norm(y).T)
-    print(xy / nor)
     return np.arccos(xy / nor)
 
 
+class TestSuite(unittest.TestCase):
+
+    def test_theta(self):
+        a = np.array([
+            [-1, 1],
+            [1, 3 ** .5]
+        ])
+        b = np.array([
+            [3 ** .5, 1],
+            [0, -1]
+        ])
+
+        theta = theta_matrix(a, b)
+        asp = np.array([
+            [7 / 12, 3 / 4],
+            [1 / 6,  5 / 6]
+        ]) * np.pi
+        npt.assert_almost_equal(theta, asp)
+
+    def test_bound(self):
+        x = np.array([
+            [-3, 2],
+            [-5.0004, 2],
+            [0.5, 4.994],
+            [0, 0.5]
+        ])
+
+        eps = 0.03
+        # If at door: |x[0]| <= 1 and |x[1]| < eps
+        door = np.expand_dims((np.abs(x - [0, 0]) < [1, eps]).prod(axis=1), -1)
+        low = np.abs(x - [-5, 0]) < eps  # at lower bound
+        upp = np.abs(x - [5, 5]) < eps  # at upper bound
+
+        asp = np.array([
+            [False, False],
+            [True, False],
+            [False, True],
+            [False, False]
+        ])
+        bdd = (~ door) & (low | upp)
+
+        npt.assert_equal(asp, bdd)
+
+
 if __name__ == "__main__":
-    x = np.array([[1, 2],
-                  [-1, 1],
-                  [2, 0.0002],
-                  [-2, -2]])
-
-    eps = 0.03
-    # If at door: |x[0]| <= 1 and |x[1]| < eps
-    door = np.expand_dims((np.abs(x - [0, 0]) < [1, eps]).prod(axis=1), -1)
-    low = np.abs(x - [-5, 0]) < eps  # at lower bound
-    upp = np.abs(x - [5, 5]) < eps  # at upper bound
-
-    bdd = (~ door) & (low | upp)
-    print(theta_matrix(x, x))
+    unittest.main()
