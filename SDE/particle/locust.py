3b7be67862ce88a44bb37b714b80481d8680cab2
oopsy <whoops.yao@gmail.com>
2020-06-23 15:16:32 +0800

    🚧 SDE Apply blind to locust

--- SDE/particle/locust.py
+++ SDE/particle/locust.py
@@ -1,6 +1,6 @@
 import numpy as np
 from progressbar import progressbar
-from utils import delta_matrix, norm
+from utils import delta_matrix, norm, blind, expand
 import matplotlib.pyplot as plt
 from celluloid import Camera
 
@@ -9,10 +9,12 @@ N = 200
 U = 1
 G = 0.5
 g = 1
-T = 20
+T = 10
+blind_consideration = True
 
 # Uniform dist in [0, 3] x [0, 3]
 x = 3 * np.random.rand(N, D)
+v = np.random.rand(N, D) - .5
 
 trace = [x[0]]
 
@@ -27,19 +29,26 @@ fig, ax = plt.subplots()
 ax.autoscale_view()
 camera = Camera(fig)
 for _ in progressbar(range(int(T / dt))):
-    # plt.scatter(*(x.T), color='black', s=5)
-    # camera.snap()
+    plt.scatter(*(x.T), color='black', s=5)
+    camera.snap()
 
     r = delta_matrix(x)
     r_norm = np.expand_dims(norm(r), axis=-1)
-    social = np.nan_to_num(q(r_norm) / r_norm * r)
-    v = social.sum(axis=1) + np.tile([U, -g], (N, 1))
+    with np.errstate(divide='ignore', invalid='ignore'):
+        social = np.nan_to_num(q(r_norm) / r_norm * r)
+
+    if blind_consideration:
+        valid = expand(blind(v, x) > np.cos(np.pi / 2))
+        v = (social * valid).sum(axis=1) + np.tile([U, -g], (N, 1))
+    else:
+        v = social.sum(axis=1) + np.tile([U, -g], (N, 1))
+
     x += v * dt
     # Offland
     x = np.clip(x, [-np.inf, 0], np.inf)
     trace.append(x[0])
 
-plt.scatter(*(x.T), color='black', s=5)
+# plt.scatter(*(x.T), color='black', s=5)
 
 _, z_trace = np.array(trace).T
 t = np.linspace(0, T, len(z_trace))
@@ -47,7 +56,6 @@ t = np.linspace(0, T, len(z_trace))
 plt.figure()
 plt.plot(t, z_trace)
 
+animation = camera.animate()
+animation.save('particle/locust-noise.mp4', fps=int(T / dt / 10))
 plt.show()
-# animation = camera.animate()
-# About 6s
-# animation.save('particle/locust-noise.mp4', fps=int(T / dt / 10))
