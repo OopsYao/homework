2dd8ef48e6881359db4b10c798ff1bc83e83350a
oopsy <whoops.yao@gmail.com>
2020-06-26 23:27:48 +0800

    🚧 SDE Exclude two-way reflect

--- SDE/particle/boxing.py
+++ SDE/particle/boxing.py
@@ -3,6 +3,7 @@ import numpy as np
 from utils import delta_matrix, norm, expand
 from progressbar import progressbar
 from celluloid import Camera
+import numpy.testing as npt
 
 # Draw the border
 # lx, ux
@@ -28,12 +29,18 @@ b = 0.2
 p = 3
 c = 0.15
 dt = 0.01
-T = 20
+T = 2
 
 multiplier = border @ [-1, 1]
 shift = border[:, 0]
 x = np.random.rand(N, 2) * multiplier + shift
 z = np.random.rand(2) * multiplier + shift
+
+
+def boundary_condition(bias):
+    eps = .01
+    return (bias <= expand(np.min(bias, axis=-1))) & (bias < eps)
+
 
 fig, ax = plt.subplots()
 ax.autoscale_view()
@@ -59,14 +66,20 @@ for t in progressbar(range(int(T / dt))):
     vz = c / N * (zx / expand(norm(zx, p))).sum(axis=0)
 
     # Boundary condition
-    eps = 0.05
+    eps = 0.01
     # If at door: |x[0]| <= 1 and |x[1]| < eps
     door = np.expand_dims(
         (np.abs(x - [0.5, 0]) < [0.05, eps]).prod(axis=1), -1)
-    low = np.abs(x - border[:, 0]) < eps  # at lower bound
-    upp = np.abs(x - border[:, 1]) < eps  # at upper bound
+
+    bias_l = np.abs(x - border[:, 0])
+    bias_u = np.abs(x - border[:, 1])
+    low = boundary_condition(bias_l)  # at lower bound
+    upp = boundary_condition(bias_u)  # at upper bound
     within = (x < border[:, 1] - [0, eps]) & (x > border[:, 0] + [0, eps])
     bdd = (~ door) & (low | upp) & np.flip(within, -1)
+
+    # Make sure no both reflect
+    npt.assert_equal(np.tile(False, N), bdd.prod(axis=-1))
 
     vx = np.where(bdd, -vx, vx)
 
